469240b960b98940f46a4ce02cbcd23960c09f4b
arlen neylon <arlen neylon>
2016-05-11 15:06:13 -0700

    setup server to serve static files.

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var port = process.env.port || 8888;
+
+app.use(express.static('public'));
+
+app.listen(port, function(){
+	console.log('listening on : ',port);
+});

1d25d1b424add111341ba4550a39ba334c913300
arlen neylon <arlen neylon>
2016-05-12 18:18:15 -0700

    basic test and travis setup

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+var aNumber = 42;
+
 var port = process.env.PORT || 8888;
 
 app.use(express.static('public'));

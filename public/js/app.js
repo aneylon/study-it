e472216cda16f04becf43d0539cee5c1b5d8d9da
Arlen Neylon <arlen.m.neylon@gmail.com>
2016-05-17 09:00:26 -0700

    add random function

--- public/js/app.js
+++ public/js/app.js
@@ -134,10 +134,19 @@ angular
 		$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
 		$scope.series = ['Series A', 'Series B'];
 		$scope.data = [
-		  [65, 59, 80, 81, 56, 55, 40],
-		  [28, 48, 40, 19, 86, 27, 90]
+			[randBetween(100), randBetween(100), randBetween(100), randBetween(100), randBetween(100), randBetween(100), randBetween(100)],
+			[randBetween(100), randBetween(100), randBetween(100), randBetween(100), randBetween(100), randBetween(100), randBetween(100)]
+		  // [65, 59, 80, 81, 56, 55, 40],
+		  // [28, 48, 40, 19, 86, 27, 90]
 		];
 		$scope.onClick = function (points, evt) {
 		  console.log(points, evt);
 		};
+		function randBetween(max,min){
+			if(min === undefined) {
+				return Math.floor(Math.random() * max);
+			} else {
+				return Math.floor(Math.random() * (max - min) + min)
+			}
+		}
 	})

e5c413a87c850ed78384a937c4926149007dee15
Arlen Neylon <arlen.m.neylon@gmail.com>
2016-08-12 12:09:21 -0700

    set up basic card functions

--- public/js/tests.js
+++ public/js/tests.js
@@ -2,24 +2,47 @@ console.log('this is a test');
 angular
   .module('tests',[])
   .controller('testCtrl',function($scope){
-    $scope.test = '';
+    $scope.card = '';
+    $scope.currentLib = '';
+    $scope.currentCard = 0;
+    $scope.hidingCard = true;
+    $scope.toggleHide = function(){
+      $scope.hidingCard = !$scope.hidingCard;
+    }
     $scope.next = function(){
       console.log('next');
+      // add one, if greater than total, reshuffle and start from beginning.
+      $scope.currentCard++;
+      if($scope.currentCard > $scope.currentLib.length - 1){
+        $scope.currentCard = 0;
+        shuffle($scope.currentLib);
+      }
+      $scope.showCurrentCard();
+    }
+    $scope.showCurrentCard = function(){
+      $scope.card = $scope.currentLib[$scope.currentCard];
     }
     $scope.prev = function(){
       console.log('prev');
     }
-    $scope.cards = [
-      {one:'one',two:'two',three:'three'},
-      {one:'a',two:'b',three:'c'},
-      {one:'you',two:'and me',three:'girl'}
+    $scope.load = function(lib){
+      console.log('loading ', lib);
+      shuffle(lib);
+      $scope.currentLib = lib;
+      $scope.showCurrentCard();
+    }
+    $scope.cardsOne = [
+      {question:'one',answer:'two',explain:'three'},
+      {question:'a',answer:'b',explain:'c'},
+      {question:'you',answer:'and me',explain:'girl'}
     ];
-    $scope.cards2 = [
-      {one:'5',two:'6',three:'7'},
-      {one:'five',two:'six',three:'seven'},
-      {one:'for',two:'sure',three:'man'}
+    $scope.cardsTwo = [
+      {question:'5',answer:'6',explain:'7'},
+      {question:'five',answer:'six',explain:'seven'},
+      {question:'for',answer:'sure',explain:'man'}
     ]
     var shuffle = function(arr){
+      console.log('shuffling');
     	arr.forEach(function(item,i,col){
     		var rand = Math.floor(Math.random() * arr.length);
     		var temp = item;
